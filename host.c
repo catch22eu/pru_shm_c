07236da895ecea8c3d6be7a872bef43c8178365c
Joseph Heller <joseph.heller.73@gmail.com>
2017-04-27 16:12:32 +0200

    Improved host.c by by using a direct pointer to memory

--- host.c
+++ host.c
@@ -5,15 +5,13 @@
 #include "prussdrv.h"
 #include "pruss_intc_mapping.h"
 
-// define which pru is used
-#define PRU_NUM 1
+#define PRU_NUM 1			// define which pru is used
+#define SHM_OFFSET 2048		// http://www.embedded-things.com/bbb/understanding-bbb-pru-shared-memory-access/
 
 int pru_init(void)
 {
 	tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
-
 	prussdrv_init();
-
 	if (prussdrv_open(PRU_EVTOUT_0))
 	{
 		return -1;
@@ -29,7 +27,6 @@ void pru_load(int pru_num, char* datafile, char* codefile)
 {
 	// load datafile in PRU memory
 	prussdrv_load_datafile(pru_num, datafile);
-
 	// load and execute codefile in PRU
 	prussdrv_exec_program(pru_num, codefile);
 }
@@ -40,18 +37,11 @@ void pru_stop(int pru_num)
 	prussdrv_exit();
 }
 
-int32_t read_int(size_t offset)
+volatile int32_t* init_prumem()
 {
 	volatile int32_t* p;
 	prussdrv_map_prumem(PRUSS0_SHARED_DATARAM, (void**)&p);
-	return p[2048 + offset];
-}
-
-void write_int(int32_t x, size_t offset)
-{
-	volatile int32_t* p;
-	prussdrv_map_prumem(PRUSS0_SHARED_DATARAM, (void**)&p);
-	p[2048 + offset] = x;
+	return p+SHM_OFFSET;
 }
 
 int main(void)
@@ -62,10 +52,18 @@ int main(void)
 	// load the PRU code (consisting of both code and data bin file).
 	pru_load(PRU_NUM, "pru_data.bin", "pru_code.bin");
 
+	// get the memory pointer to the shared data segment
+	volatile unsigned int* pruDataMem = init_prumem();
+
 	printf("sending CAFEBABE to PRU\n");
-	write_int(0xCAFEBABE,0);
+
+	// write to shared data memory 
+	pruDataMem[0]=0xCAFEBABE;
+
 	sleep(1);
-	printf("PRU replies: %X\n", read_int(1));
+
+	// read from shared data memory
+	printf("PRU replies: %X\n", pruDataMem[1]);
 
 	pru_stop(PRU_NUM);
 
