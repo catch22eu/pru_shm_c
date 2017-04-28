b81d85bbca9e1c20a68a8c5e16002f9d3e47c434
Joseph Heller <joseph.heller.73@gmail.com>
2017-04-28 10:56:12 +0200

    Minimalized pru.c by using pointer to shared memory directly

--- pru.c
+++ pru.c
@@ -2,24 +2,17 @@
 
 #define PRU_SHARED_MEM_ADDR 0x00012000
 
-void write_int(int index, int value)
-{
-	(*(volatile unsigned int *) (PRU_SHARED_MEM_ADDR + 4 * index)) = value;	
-}
-
-int read_int(int index)
-{	
- 	return (*(volatile unsigned int *) (PRU_SHARED_MEM_ADDR + 4 * index));	
-}
-
 int main(void)
 {
+	// direct pointer to memory address
+	volatile int* shared_mem = (volatile int *) PRU_SHARED_MEM_ADDR;
+
 	// enable OCP
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
-	
-	while(read_int(0)!=0xCAFEBABE) {}
-	
-	write_int(1,0xDEADBEEF);
+
+	while(shared_mem[0]!=0xCAFEBABE) {}
+
+	shared_mem[1]=0xDEADBEEF;
 
 	while(1) {}
 
